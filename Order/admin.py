e4c39905db6f6663e41f29997a1d4d7423128941
tesfayenatan@yahoo.com <ns@etechsc.com>
2021-05-09 23:36:04 +0300

    dd

--- Order/admin.py
+++ Order/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Food,Order,TotalOrder,File,FoodCategory,Delivery,OpeningAndClosing,Report,EmailAddress,PhoneNumber
 
-#admin.site.register(Food)
+admin.site.register(Food)
 #admin.site.register(Order)
 #admin.site.register(TotalOrder)
 #admin.site.register(FoodCategory)
